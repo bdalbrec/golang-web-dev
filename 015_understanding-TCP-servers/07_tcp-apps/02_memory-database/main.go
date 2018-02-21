ac8cb24531fb761dbde2856cc413b48e7a864f15
bdalbrec <brian.albrecht@gmail.com>
2018-02-20 18:56:02 -0800

    add 015_understanding-TCP-servers

---
+++ 015_understanding-TCP-servers/07_tcp-apps/02_memory-database/main.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"log"
+	"net"
+	"strings"
+)
+
+func main() {
+	li, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer li.Close()
+
+	for {
+		conn, err := li.Accept()
+		if err != nil {
+			log.Fatalln(err)
+		}
+		go handle(conn)
+	}
+}
+
+func handle(conn net.Conn) {
+	defer conn.Close()
+
+	// instructions
+	io.WriteString(conn, "\nIN-MOMORY DATABASE\n\n"+
+		"USE:\n"+
+		"SET key value \n"+
+		"GET key \n"+
+		"DEL key \n\n"+
+		"EXAMPMLE: \n"+
+		"SET fav chocolate \n"+
+		"GET fav \n\n\n")
+
+	// read & WriteString
+	data := make(map[string]string)
+	scanner := bufio.NewScanner(conn)
+	for scanner.Scan() {
+		ln := scanner.Text()
+		fs := strings.Fields(ln)
+		// logic
+		switch fs[0] {
+		case "GET":
+			k := fs[1]
+			v := data[k]
+			fmt.Fprintf(conn, "%s\n", v)
+		case "SET":
+			if len(fs) != 3 {
+				fmt.Fprintln(conn, "EXPECTED VALUE")
+				continue
+			}
+			k := fs[1]
+			v := fs[2]
+			data[k] = v
+		case "DEL":
+			k := fs[1]
+			delete(data, k)
+		default:
+			fmt.Println(conn, "INVALID COMMAND")
+		}
+	}
+}
