bb94ac0b5fcd7a39569d766f05b949f71b9ce1c8
Brian Albrecht <brian.albrecht@gmail.com>
2018-03-24 17:32:41 -0700

    finish 030 and starting 031

---
+++ 031_AWS/01_hello/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"io"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", index)
+	http.Handle("/favicon.ico", http.NotFoundHandler())
+	http.ListenAndServe(":80", nil)
+}
+
+func index(w http.ResponseWriter, req *http.Request) {
+	io.WriteString(w, "Oh yea, I'm running on AWS!")
+}
